a771201bfe3149f9dc3c1bac3aac2c519613bcb6
Suivez <suivez@interia.pl>
2024-09-11 20:40:38 +0200

    11.09.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App mx-4 sm:mx-20 lg:mx-40">
+    <div className="App mx-4 sm:mx-20 lg:mx-40 sm:h-screen">
       <Navbar />
+      <Home />
     </div>
   );
 }
